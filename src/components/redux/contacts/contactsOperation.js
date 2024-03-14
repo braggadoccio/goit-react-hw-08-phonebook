d599cb1f0eaa58de65c2fa9bbd454ca49eeec200
Paulo <pau.solanoy@gmail.com>
2024-03-14 14:55:09 +0800

    update

--- src/components/redux/contacts/contactsOperation.js
+++ src/components/redux/contacts/contactsOperation.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://65eb060b43ce164189331296.mockapi.io';
+// axios.defaults.baseURL = 'https://65eb060b43ce164189331296.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,10 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from '../redux/auth/authSlice';
 import { contactsReducer } from '../redux/contacts/contactsSlice';
 import { filterReducer } from '../redux/filter/filterSlice';
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
