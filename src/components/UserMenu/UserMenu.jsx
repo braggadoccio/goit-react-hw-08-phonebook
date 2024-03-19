e70045297144c83230c5606c1ef9bf403ebbde81
Paulo <pau.solanoy@gmail.com>
2024-03-19 21:50:52 +0800

    update

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -7,6 +7,14 @@ export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
+  // return (
+  //   <div className={css.wrapper}>
+  //     <p className={css.username}>Welcome, {user.email}</p>
+  //     <button type="button" onClick={() => dispatch(logOut())}>
+  //       Logout
+  //     </button>
+  //   </div>
+  // );
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome, {user.email}</p>
